d975c77fdc0c9d6ca1c5a673c453409a864904dc
Tatiana Ganzhinova <39487464+PesukarhuTG@users.noreply.github.com>
2018-08-17 15:44:53 +0300

    js ex 2 (FizzBuzz 3&&5)

---
+++ js ex 2 (FizzBuzz 3&&5).js
@@ -0,0 +1,5 @@
+for (var number = 1; number <101; number ++)
+{if ((number%3==0)&&(number%5==0)) console.log("fizzbuzz");
+ else if (number%5==0) console.log("buzz");
+    else if (number%3==0) console.log("fizz");
+        else console.log(number);}
