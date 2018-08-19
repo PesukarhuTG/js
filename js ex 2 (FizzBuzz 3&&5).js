eb8752d41edd0d8920a87f6f1b4164e35f0215a8
Tatiana Ganzhinova <39487464+PesukarhuTG@users.noreply.github.com>
2018-08-20 00:30:01 +0300

    js ex 2 (FizzBuzz 3&&5) ver2

--- js ex 2 (FizzBuzz 3&&5).js
+++ js ex 2 (FizzBuzz 3&&5).js
@@ -1,5 +1,5 @@
-for (var number = 1; number <101; number ++)
-{if ((number%3==0)&&(number%5==0)) console.log("fizzbuzz");
- else if (number%5==0) console.log("buzz");
-    else if (number%3==0) console.log("fizz");
-        else console.log(number);}
+for (var number = 1; number < 101; number++) 
+{var line = ""; //вводим строку
+  if (number % 3 == 0) line=line+"fizz"; //если кратно 3, к строке прибавляется fizz
+  if (number % 5 == 0) line=line+"buzz"; //если кратно 5, к строке прибавляется buzz
+console.log(line || number);} //в итоге выводим (значение строки line, попавшее под условия) ИЛИ (число)
